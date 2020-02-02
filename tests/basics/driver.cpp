75ff3f71c6796c6a516f300b4ffe88c393564638
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2020-02-02 16:21:51 +0100

    Basics working.

--- tests/basics/driver.cpp
+++ tests/basics/driver.cpp
@@ -1,33 +1,28 @@
-#include <cassert>
-#include <sstream>
-#include <stdexcept>
+#include <tabulate/table.hpp>
 
-#include <tabulate/version.hpp>
-#include <tabulate/tabulate.hpp>
+using namespace tabulate;
 
-int main ()
-{
-  using namespace std;
-  using namespace tabulate;
+int main() {
+  Table employees;
 
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
+  Table department;
+  department.add_row({"Research", "Engineering"});
 
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+  // Add rows
+  employees.add_row({"Emp. ID", "First Name", "Last Name", "Department / Business Unit"});
+  employees.add_row({"101", "Donald", "Patrick", "Finance"});
+  employees.add_row({"102", "Rachel", "Williams", "Marketing and Operational\nLogistics Planning"});
+  employees.add_row({"103", "Ian", "Jacob", department});
+
+  employees.column(0)
+      .format()
+      .font_style({FontStyle::bold})
+      .font_color(Color::white)
+      .font_align(FontAlign::right);
+
+  employees.column(3).format().font_align(FontAlign::center);
+
+  // Print the table
+  std::cout << employees << std::endl;
 }
+
